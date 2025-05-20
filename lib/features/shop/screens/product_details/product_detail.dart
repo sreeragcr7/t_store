75c3c3d961d6c18dee0405e9fe61bc1f6c6e118c
sreeragcr7 <sreeragrag373@gmail.com>
2025-05-20 16:07:56 +0530

    updated product_detais.dart, added bottom_add_to_cart.dart, product_attributes.dart and choice_chip in chips widget.

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
 import 'package:t_store/common/widgets/images/t_rounded_image.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
+import 'package:t_store/features/shop/screens/product_details/widget/bottom_add_to_cart.dart';
+import 'package:t_store/features/shop/screens/product_details/widget/product_attributes.dart';
 import 'package:t_store/features/shop/screens/product_details/widget/product_detail_image_slider.dart';
 import 'package:t_store/features/shop/screens/product_details/widget/product_meta_data.dart';
 import 'package:t_store/features/shop/screens/product_details/widget/rating_share_widget.dart';
@@ -18,6 +22,7 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -38,14 +43,40 @@ class ProductDetailScreen extends StatelessWidget {
 
                   //Price, Title, Stock & Brand
                   TProductMetaData(),
+                  const SizedBox(height: TSizes.spaceBtwItems / 2),
 
                   //Attributes
+                  TProductAttributes(),
+                  const SizedBox(height: TSizes.spaceBtwSections / 1.3),
 
                   //Checkout Button
+                  SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () {}, child: Text('Checkout'))),
+                  const SizedBox(height: TSizes.spaceBtwSections / 1.5),
 
                   //Description
+                  TSectionHeading(title: 'Description', showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  ReadMoreText(
+                    'This is a product discription for blue nike sleeve less vest. There are more things that can be added but i am jsust practicing nothing else bye have a good day..',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: 'Show more',
+                    trimExpandedText: 'Show less',
+                    moreStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
 
                   //Reviews
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TSectionHeading(title: 'Reviews(199)', onPressed: () {}, showActionButton: false),
+                      IconButton(onPressed: () {}, icon: const Icon(Iconsax.arrow_right_3, size: 18)),
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections / 2),
                 ],
               ),
             ),
---
+++ lib/features/shop/screens/product_details/widget/product_attributes.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/chips/choice_chip.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/t_rounded_container.dart';
+import 'package:t_store/common/widgets/texts/product_price_text.dart';
+import 'package:t_store/common/widgets/texts/product_title_text.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/size.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class TProductAttributes extends StatelessWidget {
+  const TProductAttributes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Column(
+      children: [
+        //Pricing & Discription
+        TRoundedContainer(
+          padding: EdgeInsets.all(TSizes.md),
+          backgroundColor: dark ? TColors.darkerGrey : TColors.grey,
+          child: Column(
+            children: [
+              //Title, Price & Stock Status
+              Row(
+                children: [
+                  TSectionHeading(title: 'Variation', showActionButton: false),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          //title
+                          const TProductTitleText(title: 'Price : ', smallSize: true),
+
+                          //Actual price
+                          Text(
+                            '\$25',
+                            style: Theme.of(
+                              context,
+                            ).textTheme.titleSmall!.apply(decoration: TextDecoration.lineThrough),
+                          ),
+                          const SizedBox(width: TSizes.spaceBtwItems),
+
+                          //Sale price
+                          const TProductPriceText(price: '20'),
+                        ],
+                      ),
+
+                      //Stock
+                      Row(
+                        children: [
+                          const TProductTitleText(title: 'Stock : ', smallSize: true),
+                          Text('In Stock', style: Theme.of(context).textTheme.titleMedium),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              //Variation description
+              const TProductTitleText(
+                title: 'This is the Description of the Product and it can go upto max 4 lines.',
+                smallSize: true,
+                maxLines: 4,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+
+        //Attributes
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+            const TSectionHeading(title: 'Colors', showActionButton: false),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            Wrap(
+              spacing: 8,
+              children: [
+                TChoiceChip(text: 'Green', selected: true, onSelected: (value) {}),
+                TChoiceChip(text: 'Blue', selected: false, onSelected: (value) {}),
+                TChoiceChip(text: 'Red', selected: false, onSelected: (value) {}),
+              ],
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TSectionHeading(title: 'Size', showActionButton: false),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            Wrap(
+              spacing: 8,
+              children: [
+                TChoiceChip(text: 'S 7', selected: true, onSelected: (value) {}),
+                TChoiceChip(text: 'S 10', selected: false, onSelected: (value) {}),
+                TChoiceChip(text: 'S 8', selected: false, onSelected: (value) {}),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
